960f3a619793159e962beb5bfb7b04dafdeb1963
NShreedesh <65639214+NShreedesh@users.noreply.github.com>
2022-07-05 09:06:00 +0545

    Applied a grid to layout products

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -4,10 +4,10 @@ function Products(props) {
   const { product } = props;
 
   return (
-    <div className="flex flex-col items-center justify-between flex-grow-0 px-10 py-10 shadow-sm cursor-pointer w-72 shadow-gray-400 rounded-xl hover:shadow-2xl hover:shadow-slate-900">
+    <div className="flex flex-col items-center justify-between gap-4 px-10 py-5 transition-all border border-gray-400 border-solid shadow-sm cursor-pointer hover:scale-x-110 hover:scale-y-110 flex-grow-1 w-72 shadow-gray-400 rounded-xl hover:shadow-xl hover:shadow-slate-900">
       <img className="w-32 h-32" src={`${product.image}`} alt="" />
-      <div className="flex items-start gap-10">
-        <p className="font-semibold">{product.title}</p>
+      <div className="flex items-start w-full gap-5">
+        <p className="font-semibold truncate">{product.title}</p>
         <p className="font-semibold text-green-700">{`$${product.price}`}</p>
       </div>
     </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import GetProducts from "../../components/apifetch/ApiFetch";
 import Products from "../../components/products/Products";
 
+import "./home.scss";
+
 function Home() {
   const [products, setProducts] = useState([]);
 
@@ -16,8 +18,8 @@ function Home() {
 
   return (
     <div class="flex flex-col items-center justify-center">
-      <h1 className="mt-5 mb-6 text-3xl font-bold">Products</h1>
-      <div className="flex flex-wrap justify-center shrink-1 gap-x-10 gap-y-10">
+      <h1 className="mt-5 mb-6 text-3xl font-bold underline">Products</h1>
+      <div className="grid grid-flow-row grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
         {products.map((product) => {
           return <Products key={product.id} product={product} />;
         })}
